c315f67521754568fe14756ccc6244b42afe118c
kercylan <61743331+kercylan98@users.noreply.github.com>
2021-11-17 11:52:44 +0800

    优化

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/kercylan98/woats
 go 1.17
 
 require (
-	github.com/kercylan98/exception v0.0.0-20210618030621-ecd8664be0e7 // indirect
-	github.com/tealeg/xlsx v1.0.5 // indirect
+	github.com/kercylan98/exception v0.0.0-20210618030621-ecd8664be0e7
+	github.com/tealeg/xlsx v1.0.5
 )
--- main.go
+++ main.go
@@ -7,28 +7,24 @@ import (
 )
 
 func main() {
-	w := woats.New(new(designer.BilingualSchoolAffiliatedToEastChinaNormalUniversity2021), 2,
-		//&strategy.Continuity{
-		//	Optimum: 3,
-		//	Min:     2,
-		//	Max:     4,
-		//},
-		//&strategy.QuantitativeCombination{
-		//	Combination:  []int{1, 12, 23, 34, 45},
-		//	Count:        1,
-		//	CheckTeacher: true,
-		//},
-		&strategy.OptimizationLink{SlotA: 3, SlotB: 4},
-		&strategy.OptimizationLink{SlotA: 14, SlotB: 15},
-		&strategy.OptimizationLink{SlotA: 25, SlotB: 26},
-		&strategy.OptimizationLink{SlotA: 36, SlotB: 37},
-		&strategy.OptimizationLink{SlotA: 47, SlotB: 48},
-		&strategy.Optimization{
-			ExcludeSlot: []int{3, 4, 14, 15, 25, 26, 36, 37, 47, 48},
+	w := woats.New(new(designer.BilingualSchoolAffiliatedToEastChinaNormalUniversity), 2,
+		&strategy.Continuity{
+			Optimum: 3,
+			Min:     2,
+			Max:     4,
 		},
-		&strategy.RandomSeek{
-			StopLoss:    0.96,
-			ExcludeSlot: []int{3, 4, 14, 15, 25, 26, 36, 37, 47, 48},
+		&strategy.QuantitativeCombination{
+			Combination:  []int{1, 12, 23, 34, 45},
+			Count:        1,
+			CheckTeacher: true,
+		},
+		//&strategy.OptimizationLink{SlotA: 3, SlotB: 4},
+		//&strategy.OptimizationLink{SlotA: 14, SlotB: 15},
+		//&strategy.OptimizationLink{SlotA: 25, SlotB: 26},
+		//&strategy.OptimizationLink{SlotA: 36, SlotB: 37},
+		//&strategy.OptimizationLink{SlotA: 47, SlotB: 48},
+		&strategy.Optimization{
+			ExcludeSlot: []int{3},
 		},
 	)
 
@@ -37,12 +33,12 @@ func main() {
 			e.Print()
 		}
 	} else {
-		if err := view.CreateClassViewExcel("DP1 Physics HL", "out"); err != nil {
-			panic(err)
-		}
-		if err := view.CreateClassViewExcel("DP1 Mathematics-Pure", "out"); err != nil {
-			panic(err)
-		}
+		//if err := view.CreateClassViewExcel("DP1 Physics HL", "out"); err != nil {
+		//	panic(err)
+		//}
+		//if err := view.CreateClassViewExcel("DP1 Mathematics-Pure", "out"); err != nil {
+		//	panic(err)
+		//}
 		if err := view.CreateAllViewExcel("out"); err != nil {
 			panic(err)
 		}
